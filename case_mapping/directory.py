7a983e008e1bc25158df8cf57979a15835f7d66d
Alex Nelson <alexander.nelson@nist.gov>
2024-03-12 16:37:11 -0400

    Add type annotations
    With these annotations, the following command (with the package
    `types-pytz` also installed) reports no issues:
    
        mypy case_mapping example.py tests
    
    Adding type review to unit testing is left for a future patch.
    
    No effects were observed on Make-managed files.
    
    References:
    * https://docs.python.org/3/library/typing.html#the-type-of-class-objects
    
    Disclaimer:
    Participation by NIST in the creation of the documentation of mentioned
    software is not intended to imply a recommendation or endorsement by the
    National Institute of Standards and Technology, nor is it intended to
    imply that any specific software is necessarily the best available for
    the purpose.
    
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

--- case_mapping/directory.py
+++ case_mapping/directory.py
@@ -1,8 +1,9 @@
+from .base import FacetEntity
 from .case import *
 from .uco import *
 
-submodules = [v for k, v in globals().items() if k[:2] != "__"]
+submodules = [v for k, v in globals().items() if k[:2] != "__" and k != "FacetEntity"]
 
-directory = dict()
+directory: dict[str, type[FacetEntity]] = dict()
 for submodule in submodules:
     directory |= submodule.directory
